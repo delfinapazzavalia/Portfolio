da6743f89dbfde3cbc3240e3c8800b8a9381c679
Delfina <delfinapazzavalia@gmail.com>
2023-06-06 10:48:45 -0300

    animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import './Home.css';
-import perfil from '../imagenes/perfildelfi.png';
+import React from "react";
+import "./Home.css";
+import perfil from "../imagenes/perfildelfi.png";
 
 const Home = () => {
   return (
     <div className="home">
+      <div className="bubbles">
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 25 }}></span>
+        <span style={{ "--i": 36 }}></span>
+        <span style={{ "--i": 21 }}></span>
+        <span style={{ "--i": 46 }}></span>
+        <span style={{ "--i": 28 }}></span>
+        <span style={{ "--i": 50 }}></span>
+        <span style={{ "--i": 33 }}></span>
+        <span style={{ "--i": 15 }}></span>
+        <span style={{ "--i": 45 }}></span>
+        <span style={{ "--i": 22 }}></span>
+        <span style={{ "--i": 39 }}></span>
+        <span style={{ "--i": 74 }}></span>
+        <span style={{ "--i": 62 }}></span>
+        <span style={{ "--i": 58 }}></span>
+      </div>
       <div className="home-left">
         <h2>WELCOME!</h2>
         <h1>I'm Delfina Paz Zavalia</h1>
@@ -17,6 +35,6 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
